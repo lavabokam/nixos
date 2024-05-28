fefaa1239ee70dab20bdf37ba009690de872e988
lava <lavajnv@gmail.com>
2024-05-28 22:04:43 +0530

    "Waybar configuration added"

---
+++ waybar/waybar.nix
@@ -0,0 +1,9 @@
+{pkgs, ...}:
+{
+  programs.waybar = {
+    enable = true;
+    package = pkgs.waybar;
+  };
+  home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
+  home.file.".config/waybar/style.css".source = ./style.css;
+}
